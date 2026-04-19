8f22fc85053c7ece278424bc3f158a951b94a5a6
Shamin Yasser <shaminbd01@gmail.com>
2026-04-20 02:00:28 +0600

    gym

---
+++ CodeForces/Gym/100307/100307 H.cpp
@@ -0,0 +1,168 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+//using u128 = __uint128_t;
+//using i128 = __int128_t;
+
+#define read()          freopen("hack.in","r",stdin)
+#define write()         freopen("hack.out","w",stdout)
+
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define endl            "\n"
+#define ll              long long
+#define double          long double
+#define ull             unsigned long long
+#define all(x)          x.begin(), x.end()
+#define rall(x)         x.rbegin(), x.rend()
+#define uniq(x)         unique(x.begin(), x.end()) - x.begin()
+#define deci(x)         cout << fixed << setprecision(x);
+#define war()           ios::sync_with_stdio(0); cin.tie(0);
+
+const ll LLMax = 1e18;
+const ll LLMin = -1e18;
+const ll MOD   = 1e9+7;
+const double PI = acos(-1);       //3.1415926535897932384626
+
+int dx[] = {-1, 1,  0, 0, -1, -1,  1, 1};
+int dy[] = { 0, 0, -1, 1, -1,  1, -1, 1};
+
+//pbds
+template<typename T>using ordered_set=tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//template<typename T>using ordered_multiset=tree<T,null_type,less_equal<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+//debug
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define db(x)
+#endif
+
+//sc
+template<typename T1> istream &operator>>(istream &cin, vector<T1> &a) { for (auto &x : a) cin >> x; return cin; }
+
+// nCr = n! / r! * (n-r)!
+// nPr = n! / (n-r)!
+// .order_of_key(x)
+// *.find_by_order(x)
+//substr
+//cin.ignore();
+//get<idx>(z) //tuple
+//__builtin_popcount()
+//memset(dp, -1, sizeof(dp));
+//cout<<"Case "<<T<<": ";
+
+#define YES {cout<<"YES"<<endl;}
+#define NO  {cout<<"NO"<<endl;}
+#define IMP {cout<<"-1"<<endl;}
+
+
+///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE
+///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE
+///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE
+///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE
+///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE
+///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE            ///MAIN CODE
+
+#define int ll
+const int N=1e5+7;
+const int bit=32;
+int arr[N];
+int XOR[N];
+int treee[bit][4*N];
+map<int,ordered_set<int>>mp;
+
+void build(int node, int st, int en, int id){
+    if(st==en){  
+        treee[id][node] = ((arr[st]>>id)&1) ? LLMax : st;
+        return;
+    }
+    int mid=((en-st)/2)+st;
+    build(2*node, st, mid, id);
+    build(2*node+1, mid+1, en, id);
+    treee[id][node]=min(treee[id][2*node],treee[id][2*node+1]);
+    return;
+}
+
+
+int query(int node, int st, int en, int l, int r, int id){
+    if(st>r || en<l){return LLMax;}
+    else if(st>=l && en<=r){return treee[id][node];}
+    int mid=((en-st)/2)+st;
+    int q1= query(2*node, st, mid, l, r, id);
+    int q2= query(2*node+1, mid+ 1, en, l, r, id);
+    return min(q1,q2);
+}
+
+
+void solve(int T){
+    int n;  cin>>n;
+    for(int i=1; i<=n; i++){cin>>arr[i];}
+    for(int i=1; i<=n; i++){
+        XOR[i]=XOR[i-1]^arr[i];
+        mp[XOR[i]].insert(i);
+    }
+
+    for(int i=0; i<bit; i++){
+        build(1, 1, n, i);
+    }
+
+
+
+    int ans=0;
+    for(int i=1; i<=n; i++){
+        map<int,vector<int>>ok;
+        for(int id=0; id<bit; id++){
+            int idx=query(1,1,n,i,n,id);
+            if(idx<=n){
+                ok[idx].pb(id);
+            }
+        }
+        ok[n+1].pb(68);
+        int s_idx=i;
+        int val=(1ll<<bit)-1;
+        for(auto &[u,v]: ok){
+            int e_idx=u;
+            int x_o_r=val^XOR[i-1];
+            ans+=mp[x_o_r].order_of_key(e_idx);
+            ans-=mp[x_o_r].order_of_key(s_idx);
+            for(auto z:v){
+                val^=(1ll<<z);
+            }
+            s_idx=e_idx;
+        }
+    }
+    cout<<ans<<endl;
+
+} 
+
+
+
+    
+
+int32_t main(){             //DRINK WATER
+    war();
+    read();   write();
+    ////////////////////////////////////
+
+    ////////////////////////////////////
+    int tc=1;
+    //cin>>tc;
+    for(int T=1; T<=tc; T++){
+        solve(T);
+    }
+return 0;
+} 
+
+/*
+
+     /\_/\
+    (= ._.)
+    / >  \> Mr_Warlock
+   /      \
+
+*/
